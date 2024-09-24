84a25748da6c7f4a3c16c691cc0e741db122fbd3
Your Name <youremail@domain.com>
2024-09-24 15:56:46 +0300

    creating function 3

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function() {
+    return 2000 / 100;
+};
+
+// Arrow function for square
+const square = (num) => {
+    return num * num;
+};
+
+// Arrow function for add
+const add = (a, b) => {
+    return a + b;
+};
